94c3aa221280096ed7543b0d2020c39176486c93
Joseph Furlott <jmfurlott@gmail.com>
2015-03-31 23:10:52 -0400

    ENH: build tasks, index going | CLN: removed node_modules

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+
+var React = _interopRequire(require("react"));
+
+var Table = _interopRequire(require("./Table.js"));
+
+React.render(React.createElement(Table, null), document.getElementById("app"));
---
+++ src/app.js
@@ -0,0 +1,6 @@
+'use strict';
+import React from 'react';
+
+import Table from "./Table.js";
+
+React.render( <Table />, document.getElementById("app"));
